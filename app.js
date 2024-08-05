c74d3d91c0dd84aeb8fa14c6eabebd8405ca22b7
Rahat <rahatalisoomro10@gmail.com>
2024-08-05 13:29:42 +0500

    Undid some changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// jshint esversion:6
+//jshint esversion:6
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,23 +8,23 @@ require('dotenv').config();
 
 const app = express();
 
-app.set('view engine', 'ejs'); // Set the view engine
-app.set('views', __dirname + '/views'); // Set the views directory
+app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_CONNECT_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_CONNECT_URI, { useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.log('MongoDB connection error:', err));
 
-// Define Mongoose Schemas
-const itemsSchema = new mongoose.Schema({
-  name: String
-});
 
-const Item = mongoose.model("Item", itemsSchema);
+
+
+ const itemsSchema = new mongoose.Schema({
+  name: String
+ });
+
+const Item = mongoose.model("Item",itemsSchema);
 
 const item1 = new Item({
   name: "Welcome to your todo list."
@@ -45,102 +45,112 @@ const listSchema = {
   items: [itemsSchema]
 };
 
-const List = mongoose.model("List", listSchema);
+const List = mongoose.model("list", listSchema);
 
-// Routes
-app.get("/", async (req, res) => {
-  try {
-    const items = await Item.find();
-    if (items.length === 0) {
-      await Item.insertMany(defaultItems);
-      console.log("Default items added to DB");
-      res.redirect('/');
-    } else {
-      res.render("list", { listTitle: "Today", newListItems: items });
+
+app.get("/", function(req, res) {
+  (async()=>{
+    try{
+      const items = await Item.find();
+      if(items.length === 0){
+        Item.insertMany(defaultItems).then(function(){
+          console.log("Data inserted")
+        }).catch(function(err){
+          console.log(err);
+        });
+        res.redirect('/');
+      } else{
+        res.render("list", {listTitle: "Today", newListItems: items});
+      }
+    } catch(err){
+      console.log(err);
     }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
+  })()
+  
 });
 
-app.get("/:customListName", async (req, res) => {
+app.get("/:customListName", function(req, res){
+  
   const customListName = _.capitalize(req.params.customListName);
-  try {
-    const foundList = await List.findOne({ name: customListName });
-    if (!foundList) {
-      const list = new List({
-        name: customListName,
-        items: defaultItems
-      });
-      await list.save();
-      res.redirect("/" + customListName);
-    } else {
-      res.render("list", { listTitle: foundList.name, newListItems: foundList.items });
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
+
+  List.findOne({name: customListName})
+    .then((docs)=>{
+      if(!docs){
+        // Create a new list
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        res.redirect("/"+customListName);
+      }else{
+        // Show an existing list
+        res.render("list", {listTitle: docs.name, newListItems: docs.items});
+      }
+      
+    })
+    .catch((err)=>{
+      console.log(err);
+    });
+  // console.log(found);
+  
+  
+
+
+
 });
 
-app.post("/", async (req, res) => {
+app.post("/", function(req, res){
   const itemName = req.body.newItem;
   const listName = req.body.list;
+  
+  const item = new Item({
+    name: itemName
+  });
 
-  const item = new Item({ name: itemName });
-
-  if (listName === "Today") {
-    await item.save();
+  if(listName === "Today"){
+    item.save();
     res.redirect("/");
-  } else {
-    try {
-      const foundList = await List.findOne({ name: listName });
-      foundList.items.push(item);
-      await foundList.save();
-      res.redirect("/" + listName);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
+  }else{
+    List.findOne({name: listName})
+      .then((foundList)=>{
+        foundList.items.push(item);
+        foundList.save();
+        res.redirect("/"+ listName);
+      })
   }
+
+  
 });
 
-app.post("/delete", async (req, res) => {
+app.post("/delete", function(req, res){
   const checkedItemId = req.body.checkbox;
   const listName = req.body.listName;
-
-  if (listName === "Today") {
-    try {
+  if (listName === "Today"){
+    (async() => {
       await Item.findByIdAndDelete(checkedItemId);
-      res.redirect("/");
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
-  } else {
-    try {
-      await List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } });
-      res.redirect("/" + listName);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Internal Server Error');
-    }
+    })()
+  
+    res.redirect("/")
+  }else{
+    List.findOneAndUpdate({name: listName},{$pull: {items: {_id: checkedItemId}}})
+      .then((foundList)=>{
+        res.redirect("/"+listName);
+      })
   }
+  
 });
 
-app.get("/about", (req, res) => {
+app.get("/about", function(req, res){
   res.render("about");
+});
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something broke!');
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke! Error: ' + err.message);
 });
